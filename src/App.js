2ccc7843056c7924a0b4eaa5c4aafc52340e9a6e
rochafialvin <rochafi.dev@gmail.com>
2022-03-03 22:44:49 +0700

    Get data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get("http://localhost:2022/users");
+      setUsers(data.data);
+    } catch (error) {
+      console.log({ error });
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,9 +27,15 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          style={{ marginBottom: 10 }}
         >
           Learn React
         </a>
+        {users.map((user) => (
+          <p style={{ marginBlock: 1, textAlign: "left" }}>
+            id: {user.id}, name: {user.name}, email: {user.email}
+          </p>
+        ))}
       </header>
     </div>
   );
